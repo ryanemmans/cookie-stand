426690c3315bea2de2e408a2005705a5222f925f
Ryan Emmans <ryanemmans@gmail.com>
2021-07-22 21:23:35 -0700

    working on rendering the form inputs

--- js/app.js
+++ js/app.js
@@ -4,12 +4,13 @@
 
 // I need to get a reference to where on the page I am putting stuff
 const tableElem = document.getElementById('sales');
+//find out where we need to listen for an event
+const formElem = document.getElementById('addStoreForm');
 // for hours of operation
 const hoursOfOperation = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
 // ----------------------------------- Constructor Functions----------------------------------- //
 
-// minCust, maxCust, avgCookiePerSale, locationName
 function Store(minCust, maxCust, avgCookiePerSale, name) {
   this.name = name;
   this.minCust = minCust;
@@ -17,12 +18,13 @@ function Store(minCust, maxCust, avgCookiePerSale, name) {
   this.avgCookiePerSale = avgCookiePerSale;
   this.salesHourly = [];
 
-  Store.allStores.push(this)
+  Store.allStores.push(this);
 }
 
 // ----------------------------------- Prototype Methods ----------------------------------- //
 
 Store.allStores = [];
+
 // getting random customer
 Store.prototype.randomCustInRange = function () {
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
@@ -34,8 +36,7 @@ Store.prototype.calculateSalesPerHour = function () {
     this.salesHourly.push(thisHourSale);
   }
 }
-// prototype method for render()
-// create a row, insert a th with the location name, insert salesHourly array values as td's, insert a th with grand total
+
 Store.prototype.renderStore = function (bodyElem) {
   let grandTotal = 0;
   const rowElem = document.createElement('tr');
@@ -78,9 +79,14 @@ function renderHeader() {
   makeElement('th', rowElem, 'Daily Location Total');
 }
 
-// loops through and renders all locations - maybe make a tbody??
+function getAllStores() { // ?????????????
+  for (let i = 0; i < Store.allStores.length; i++) {
+    let currentStore = Store.allStores[i];
+    currentStore.getAllStores();
+  }
+}
+// loops through and renders all locations
 function rendersAllStores() {
-  // create the tbody and append it to the table
   const bodyElem = makeElement('tbody', tableElem, null)
   for (let i = 0; i < Store.allStores.length; i++) {
     let currentStore = Store.allStores[i];
@@ -90,21 +96,16 @@ function rendersAllStores() {
 }
 // renders footer
 function renderFooter() {
-  // make tfoot element - give it a variable
-  // append tfoot to table
   const footerElem = makeElement('tfoot', tableElem, null);
-  // make a row - append row to tfoot
   const rowElem = makeElement('tr', footerElem, null);
-  // hourly total - make a variable
   let hourlyTotal = 0;
-  // table grand total
   let grandTotal = 0;
   makeElement('th', rowElem, 'Hourly Totals:');
   // first access the hour
   for (let i = 0; i < hoursOfOperation.length; i++) {
     // then look at the store (each store) at that hour
     for (let j = 0; j < Store.allStores.length; j++) {
-      // get the store at the hours value and add it to hourly total
+      // get the store at the hour's value and add it to hourly total
       let storesSalesAtHour = Store.allStores[j].salesHourly[i];
       hourlyTotal += storesSalesAtHour;
     }
@@ -119,6 +120,35 @@ function renderFooter() {
   makeElement('td', rowElem, grandTotal);
 }
 
+// ---------- What the heck is going on here ------------
+
+// function getAllStores() {
+//   for (let i = 0; i < Store.allStores.length; i++) {
+//     let currentStore = Store.allStores[i];
+//     currentStore.getAllStores();
+//   }
+// }
+
+// ------------------------------------------------------
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(e);
+  let name = e.target.userName.value;
+  let minCust = e.target.minCust.value;
+  let maxCust = e.target.maxCust.value;
+  let avgCookiePerSale = e.target.avgCookiePerSale.value;
+  
+  let newStore = new Store(name, minCust, maxCust, avgCookiePerSale);
+  newStore.getTotal();
+  newStore.render();
+}
+
+
+// listener here
+formElem.addEventListener('submit', handleSubmit);
+
+
 // ----------------------------------- Call Functions ----------------------------------- //
 
 const seattle = new Store(23, 65, 6.3, 'Seattle');
@@ -127,13 +157,14 @@ const dubai = new Store(11, 38, 3.7, 'Dubai');
 const paris = new Store(20, 38, 2.3, 'Paris');
 const lima = new Store(2, 16, 4.6, 'Lima');
 
+// getTotal();
 renderHeader();
 rendersAllStores();
 renderFooter();
 
-console.log(seattle);
-console.log(tokyo);
-console.log(dubai);
-console.log(paris);
-console.log(lima);
-console.log(tableElem);
+// console.log(seattle);
+// console.log(tokyo);
+// console.log(dubai);
+// console.log(paris);
+// console.log(lima);
+// console.log(tableElem);
